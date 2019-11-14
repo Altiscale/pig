201a8e16b3e3e8cc03ec8963fe79a299e9974ab7
Gustavo Schaab <gustavo.schaab@sap.com>
2019-11-14 08:47:30 -0300

    feat(BDSBLUE-476): fix shell command

--- sap-xmake-build.sh
+++ sap-xmake-build.sh
@@ -6,8 +6,6 @@ cd "$SCRIPT_DIR" &> /dev/null
 MY_DIR="$(pwd)"
 echo "[INFO] Executing in ${MY_DIR}"
 
-cd ${MY_DIR}/pig
-
 ant -Dforrest.home=/opt/apache-forrest -Dant.home=/opt/apache-ant/ -Dversion=${ARTIFACT_VERSION} -Dhadoopversion=23 clean jar
 pushd contrib/piggybank/java
 ant -Dhadoopversion=23
